e00e2359146977471dcb04588631a626f30d9f0f
4929433 <linxuan.ye@student.uq.edu.au>
2025-03-20 14:22:25 +1000

    axyz
    sdsgdt

---
+++ work_phase_1/js/modules/logging.js
@@ -0,0 +1,9 @@
+/**
+ * Module: logging
+ * Generic logging using console.log
+ */
+
+function logMessage(pagename, message) {
+    console.log(`From {pageName}: {message}`);
+}
+export { logMessage };
